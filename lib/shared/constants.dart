a672546f7d8251dc688ba0e87919dfda461cb122
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2023-05-11 15:36:12 -0300

    feat: add constants.dart

---
+++ lib/shared/constants.dart
@@ -0,0 +1,7 @@
+const kSplashImage = 'assets/images/dungeon meowster.jpg';
+
+const kAuthBackground = 'assets/images/background.jpg';
+
+const kMainBackground = 'assets/images/background 2.jpg';
+
+const kRules = 'assets/images/rules.jpg';
