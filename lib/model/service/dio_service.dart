6659bb1548f6032bc8a3868cb3fad1b4fec3fd3a
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2023-05-15 15:54:18 -0300

    refactor: remove unused condition

--- lib/model/service/dio_service.dart
+++ lib/model/service/dio_service.dart
@@ -8,7 +8,7 @@ class DioService {
           await dio.post('http://206.189.206.44:8080/login', data: body);
       if (response.statusCode == 200) {
         return response.data.toString();
-      }
+      } else {}
     } catch (e) {
       throw Exception(e);
     }
