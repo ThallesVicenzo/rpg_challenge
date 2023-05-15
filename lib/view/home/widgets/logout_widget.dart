9fc1cf65cdd7397fa122fa37cbaf0948b5a47970
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2023-05-15 15:53:19 -0300

    feat: create LogoutWidget

---
+++ lib/view/home/widgets/logout_widget.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:rpg_challenge/view_model/providers/home_provider.dart';
+import 'package:rpg_challenge/view_model/providers/splash_screen_provider.dart';
+
+class LogoutWidget extends StatelessWidget {
+  const LogoutWidget(
+      {super.key,
+      required this.theme,
+      required this.homeProvider,
+      required this.splashScreenProvider});
+
+  final ThemeData theme;
+  final HomeProvider homeProvider;
+  final SplashScreenProvider splashScreenProvider;
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: Text(
+        'Tem certeza que deseja fazer logout?',
+        style: theme.textTheme.bodyMedium,
+      ),
+      actions: [
+        TextButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: Text(
+            'Cancelar',
+            style: theme.textTheme.bodyMedium,
+          ),
+        ),
+        TextButton(
+          onPressed: () {
+            homeProvider.logout(context, splashScreenProvider);
+          },
+          child: Text(
+            'Confirmar',
+            style: theme.textTheme.bodyMedium,
+          ),
+        )
+      ],
+    );
+  }
+}
