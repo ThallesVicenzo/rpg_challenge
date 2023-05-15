08b9baf406148cafeee692562212cf2ff4eee00d
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2023-05-15 15:54:44 -0300

    feat: create MultiProvider on Parent widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:rpg_challenge/shared/theme.dart';
+import 'package:rpg_challenge/view_model/providers/splash_screen_provider.dart';
 import 'package:rpg_challenge/view_model/routes/named_routes.dart';
 import 'package:rpg_challenge/view_model/routes/routes.dart';
 
 void main() {
-  runApp(const RpgChallenge());
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) => SplashScreenProvider(),
+        )
+      ],
+      child: const RpgChallenge(),
+    ),
+  );
 }
 
 class RpgChallenge extends StatelessWidget {
