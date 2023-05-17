57715f087860be37dc3827aa26db05bd2a21aaef
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2023-05-17 16:13:42 -0300

    feat: add new image

--- lib/shared/constants.dart
+++ lib/shared/constants.dart
@@ -4,6 +4,8 @@ const kAuthBackground = 'assets/images/background.jpg';
 
 const kMainBackground = 'assets/images/background 2.jpg';
 
+const kLoading = 'assets/images/loading.png';
+
 const kRules = 'assets/images/rules.jpg';
 
 const kDefaultFontFamily = 'PermanentMarker-Regular';
