4d659cac6d254bf9eff4e66f6d09aeabd885572a
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2023-05-17 16:16:19 -0300

    feat: create sendData & getGamesList methods

--- lib/view_model/repositories/api_repository.dart
+++ lib/view_model/repositories/api_repository.dart
@@ -1,12 +1,28 @@
+import 'package:rpg_challenge/model/models/games_list.dart';
+import 'package:rpg_challenge/model/models/user.dart';
 import 'package:rpg_challenge/model/service/dio_service.dart';
+import 'package:rpg_challenge/view_model/repositories/local_data_base_repository.dart';
+
+import '../../model/models/games_content.dart';
 
 class ApiRepository {
-  static Future<String?> defineUri(String email, String password) async {
-    String? response = await DioService.postUser(body: {
-      'email': email,
-      'senha': password,
-    });
+  static Future<String?> sendData(String email, String password) async {
+    String? response = await DioService().postUser(
+      body: UserModel(email: email, password: password).toJson(),
+    );
 
     return response;
+  }
+
+  Future<List<GamesContentModel>> getGamesList(int pagina) async {
+    final token = await LocalDataBaseRepository.callRetrieveData();
+
+    final Map<String, dynamic>? response =
+        await DioService().getGamesList(token: token!, pagina: pagina);
+
+    final List<GamesContentModel> gamesList =
+        GamesListModel.fromJson(response!).content;
+
+    return gamesList;
   }
 }
