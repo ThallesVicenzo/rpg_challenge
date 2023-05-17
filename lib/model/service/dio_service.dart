ee9e9be91af40781b896a375b99de6016b8a3ba6
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2023-05-17 16:14:52 -0300

    feat: move _dio to class value & add getGamesList method

--- lib/model/service/dio_service.dart
+++ lib/model/service/dio_service.dart
@@ -1,14 +1,33 @@
 import 'package:dio/dio.dart';
 
 class DioService {
-  static Future<String?> postUser({Map<String, String>? body}) async {
+  final _dio = Dio();
+
+  Future<String?> postUser({Map<String, String>? body}) async {
     try {
-      final dio = Dio();
       final response =
-          await dio.post('http://206.189.206.44:8080/login', data: body);
+          await _dio.post('http://206.189.206.44:8080/login', data: body);
       if (response.statusCode == 200) {
-        return response.data.toString();
-      } else {}
+        return response.data;
+      }
+    } catch (e) {
+      throw Exception(e);
+    }
+    return null;
+  }
+
+  Future<Map<String, dynamic>?> getGamesList(
+      {required String token, required int pagina}) async {
+    try {
+      final response = await _dio.get(
+        'http://206.189.206.44:8080/api/jogo?page=$pagina',
+        options: Options(
+          headers: {'Authorization': token},
+        ),
+      );
+      if (response.statusCode == 200) {
+        return response.data;
+      }
     } catch (e) {
       throw Exception(e);
     }
