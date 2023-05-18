cbf6fc605e54496d9362bed12da738d14c6303f6
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2023-05-18 19:32:21 -0300

    feat: create GameResumeInfo widget

---
+++ lib/view/details/widgets/game_resume_info.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+import '../../../view_model/providers/details_provider.dart';
+
+class GameResumeInfo extends StatelessWidget {
+  const GameResumeInfo({
+    super.key,
+    required this.mediaQuery,
+    required this.theme,
+    required this.detailsProvider,
+  });
+
+  final MediaQueryData mediaQuery;
+  final ThemeData theme;
+  final DetailsProvider detailsProvider;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Image.network(
+              detailsProvider.gameDetails!.urlCapa,
+              scale: 1.2,
+            ),
+            Text(
+              detailsProvider.gameDetails!.nome,
+              style: theme.textTheme.titleLarge,
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Text(
+              '📅 ${detailsProvider.gameDetails!.ano}',
+              softWrap: false,
+              maxLines: 3,
+              style: theme.textTheme.bodyMedium!.copyWith(
+                color: theme.primaryColor,
+                fontSize: 20,
+              ),
+            ),
+            Text(
+              '🧙‍♂️ de ${detailsProvider.gameDetails!.minimoJogadores} até ${detailsProvider.gameDetails!.maximoJogadores} pessoas',
+              style: theme.textTheme.bodyMedium!
+                  .copyWith(color: theme.primaryColor, fontSize: 20),
+            ),
+            Text(
+              '🕒 ${detailsProvider.gameDetails!.duracaoMedia} min',
+              style: theme.textTheme.bodyMedium!.copyWith(
+                color: theme.primaryColor,
+                fontSize: 20,
+              ),
+            )
+          ],
+        ),
+      ],
+    );
+  }
+}
