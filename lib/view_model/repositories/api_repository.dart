1a638522eaaa7aa5c524e0841e9e14942c998bcb
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2023-05-18 19:34:33 -0300

    feat: rename methods & add convertJsonToGamesDetailsModel method

--- lib/view_model/repositories/api_repository.dart
+++ lib/view_model/repositories/api_repository.dart
@@ -1,3 +1,4 @@
+import 'package:rpg_challenge/model/models/game_details.dart';
 import 'package:rpg_challenge/model/models/games_list.dart';
 import 'package:rpg_challenge/model/models/user.dart';
 import 'package:rpg_challenge/model/service/dio_service.dart';
@@ -6,7 +7,8 @@ import 'package:rpg_challenge/view_model/repositories/local_data_base_repository
 import '../../model/models/games_content.dart';
 
 class ApiRepository {
-  static Future<String?> sendData(String email, String password) async {
+  static Future<String?> sendModelAndAwaitToken(
+      String email, String password) async {
     String? response = await DioService().postUser(
       body: UserModel(email: email, password: password).toJson(),
     );
@@ -14,7 +16,8 @@ class ApiRepository {
     return response;
   }
 
-  Future<List<GamesContentModel>> getGamesList(int pagina) async {
+  static Future<List<GamesContentModel>> convertJsonToGamesContentModel(
+      int pagina) async {
     final token = await LocalDataBaseRepository.callRetrieveData();
 
     final Map<String, dynamic>? response =
@@ -24,5 +27,18 @@ class ApiRepository {
         GamesListModel.fromJson(response!).content;
 
     return gamesList;
+  }
+
+  static Future<GamesDetailsModel> convertJsonToGamesDetailsModel({
+    required int gameId,
+  }) async {
+    final token = await LocalDataBaseRepository.callRetrieveData();
+
+    final Map<String, dynamic>? response =
+        await DioService().getGameDetails(gameId: gameId, token: token!);
+
+    final GamesDetailsModel gameDetails = GamesDetailsModel.fromJson(response!);
+
+    return gameDetails;
   }
 }
