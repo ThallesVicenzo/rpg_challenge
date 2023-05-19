a526d435835ca44fb5e79fab05cb319d15e27842
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2023-05-19 15:07:13 -0300

    feat: create PagesButton

--- lib/view/home/widgets/buttons/pages_button.dart
+++ lib/view/home/widgets/buttons/pages_button.dart
@@ -1,36 +1,44 @@
 import 'package:flutter/material.dart';
 import 'package:rpg_challenge/view_model/providers/home_provider.dart';
-import 'package:rpg_challenge/view_model/providers/pages_provider.dart';
 
-class SeeMoreButton extends StatelessWidget {
-  const SeeMoreButton({
+class PagesButton extends StatelessWidget {
+  const PagesButton({
     super.key,
-    required this.pagesProvider,
     required this.themeData,
     required this.mediaQueryData,
     required this.homeProvider,
     required this.onTap,
+    required this.title,
+    this.isCurrentPage = false,
   });
 
-  final PagesProvider pagesProvider;
+  final String title;
   final HomeProvider homeProvider;
   final ThemeData themeData;
   final MediaQueryData mediaQueryData;
   final VoidCallback onTap;
+  final bool isCurrentPage;
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
       onTap: onTap,
       child: Container(
+        height: mediaQueryData.size.height * 0.050,
+        width: mediaQueryData.size.width * 0.1,
         decoration: BoxDecoration(
           color: themeData.primaryColor,
           borderRadius: BorderRadius.circular(8),
         ),
         child: Center(
           child: Text(
-            'Ver mais',
-            style: themeData.textTheme.bodyMedium,
+            title,
+            style: themeData.textTheme.bodyMedium!.copyWith(
+              fontSize: 20,
+              color: isCurrentPage == true
+                  ? themeData.scaffoldBackgroundColor
+                  : const Color(0xFF000000),
+            ),
           ),
         ),
       ),
